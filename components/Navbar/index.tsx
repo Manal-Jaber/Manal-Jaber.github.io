2f6f3d8dfd7d5ee8841e53387cfb688034531cd0
Manal Jaber-Shehayeb <manal.jabershehayeb@mirumagency.com>
2023-01-15 21:49:08 +0200

    :sparkles: Added navbar
    Began designing Navbar

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,6 +2,9 @@
 import styles from './index.module.scss'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
+import webClipper from "assets/images/page-clipper.png";
+import code from "assets/images/code.png";
+import Image from 'next/image';
 
 export default function Navbar() {
 
@@ -21,6 +24,9 @@ export default function Navbar() {
           </Link>
         </li>
       </ul>
+      <div className={styles.menu}>
+        <Image src={code} />
+      </div>
     </div>
   )
 }
