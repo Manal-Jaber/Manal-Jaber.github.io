55e5c4bee6a4e3ad4020bd107992de7cdb3f29c8
Manal Jaber-Shehayeb <manal.jabershehayeb@mirumagency.com>
2024-03-23 00:43:29 +0200

    Fixed projects page

--- components/ProjectDisplayComponent/index.tsx
+++ components/ProjectDisplayComponent/index.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 import styles from './index.module.scss'
 import DynamicImage from 'utilities/DynamicImage';
+import clsx from 'clsx';
 
-export default function ProjectDisplayComponent({data={image: {imageName: "", alt: "", width: 500, height: 500}, title: "", description: "", slug:""}}) {
-  const {image, title, description, slug} = data;
+export default function ProjectDisplayComponent({data={image: {imageName: "", alt: "", width: 500, height: 500}, title: "", description: "", slug:"", row: true}}) {
+  const {image, title, description, slug, row} = data;
   const { imageName, alt, width, height } = image;
 
   return (
-    <Link href={`/projects/${slug}`} className={styles.projectDisplayComponent}>
+    <Link href={`/projects/${slug}`} className={clsx(styles['projectDisplayComponent'],row&& styles['row'])}>
       <DynamicImage imageName={`projects/${imageName}`} customClassName={styles.image} alt={alt} width={width} height={height} />
       <div className={styles.text}>
         <h2 className={styles.title}>{title}</h2>
