1d93c818c612402bd21a3af791bf7e8b769347a7
Manal Jaber-Shehayeb <manal.jabershehayeb@mirumagency.com>
2024-03-23 00:46:45 +0200

    Fixed ts error

--- components/Header/MobileNavigation/index.tsx
+++ components/Header/MobileNavigation/index.tsx
@@ -7,6 +7,7 @@ import clsx from 'clsx';
 export default function Intro({openHeader, setOpenHeader}: {openHeader: boolean, setOpenHeader: any}) {
 
   return (
+    //@ts-ignore
     <div id="mobile-navigation" className={clsx(styles['mobileNavigation'], openHeader&& styles['opened'])} onClick={()=> setOpenHeader(prev => !prev)}>
       {openHeader?
       <FontAwesomeIcon className={styles.icon} icon={iconMap['close']}/>
