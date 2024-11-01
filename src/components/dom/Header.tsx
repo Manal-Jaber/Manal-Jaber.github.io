60361a2cb74e543cad51964244559eb97da4ef07
Manal Jaber Shehayeb <manal.jabershehayeb@vml.com>
2024-11-01 22:13:28 +0200

    Fixed header fixed position on scroll down

--- src/components/dom/Header.tsx
+++ src/components/dom/Header.tsx
@@ -1,8 +1,30 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Header = () => {
-    return <header className="fixed top-0 z-50 flex w-full flex-row items-center justify-between bg-header p-4 md:px-10">
+
+    // Handling scroll events
+    const [isScrollingUp, setIsScrollingUp] = useState(false);
+    const [prevScrollY, setPrevScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+            if (currentScrollY < prevScrollY) {
+                setIsScrollingUp(true);
+            } else {
+                setIsScrollingUp(false);
+            }
+            setPrevScrollY(currentScrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [prevScrollY]);
+
+    return <header className={`${isScrollingUp ? 'fixed' : 'absolute'} top-0 z-50 flex w-full flex-row items-center justify-between bg-header p-4 md:px-10`}>
         <Link href='/' className="flex flex-row items-center gap-4">
             <h1 className="font-Preahvihear text-xl ">Manal Jaber</h1>
             <Image src={'/avatars/manal.webp'} alt="manal" width={60} height={60} />
