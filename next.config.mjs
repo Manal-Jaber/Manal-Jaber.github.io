3933b67ed98e38df68acbe7b2e0394b27cfdea9a
Manal-Jaber <manal.jaber94@gmail.com>
2025-01-16 03:11:50 +0200

    Nextconfig for github actiond

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath:
+    process.env.NODE_ENV === "production" ? "/Manal-Jaber.github.io" : "",
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "Manal-Jaber.github.io" : "",
+};
